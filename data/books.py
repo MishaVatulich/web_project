df5a4cae832836dbd94df229380ff8f948fa10cd
MishaVatulich <57654804+MishaVatulich@users.noreply.github.com>
2020-04-08 15:55:30 +1000

    Big commit
    Новые поля в таблице книг

--- data/books.py
+++ data/books.py
@@ -10,8 +10,11 @@ class Books(SqlAlchemyBase):
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
     title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    discription = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    description = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     cost = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
     user_id = sqlalchemy.Column(sqlalchemy.Integer,
                                 sqlalchemy.ForeignKey("users.id"))
+    genre = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    image = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    amount = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
     user = orm.relation('User')
